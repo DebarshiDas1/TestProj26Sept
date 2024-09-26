fce3b5ec416c6a23c3605636392eef92cc75ae21
DebarshiDas1 <94341651+DebarshiDas1@users.noreply.github.com>
2024-09-26 11:10:55 +0530

    Upload files from CodeZen

---
+++ API/Helpers/BaseApiController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using TestProj26Sept.Helpers.Extensions;
+
+namespace TestProj26Sept.Helpers
+{
+    /// <summary>
+/// Base class of API controller
+/// </summary>
+    public class BaseApiController : ControllerBase
+    {
+        /// <summary>
+/// User id
+/// </summary>
+        protected Guid UserId { get => HttpContext.User.Identity.GetUserId(); }
+        /// <summary>
+/// Tenant id
+/// </summary>
+        protected Guid TenantId { get => HttpContext.User.Identity.GetTenantId(); }
+    }
+}
